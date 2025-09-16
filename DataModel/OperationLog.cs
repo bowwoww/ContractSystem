bc3fd597882e9e35e9ecbc9bb338c6c639ab6448
bowwoww <rabbit88823@gmail.com>
2025-09-16 11:29:01 +0800

    操作紀錄model

---
+++ DataModel/OperationLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel
+{
+    public class OperationLog
+    {
+        public int Id { get; set; }
+        public string MemberId { get; set; } = null!;
+        public string Action { get; set; } = null!;
+        public string Target { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string IpAddress { get; set; } = null!;
+    }
+}
